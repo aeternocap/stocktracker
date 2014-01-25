918297c51cfa6738590471a1d747cb6c2d0ea3b7
Matt Menefee <matt.menefee@extractablemedia.com>
2014-01-25 14:39:39 -0500

    Change market_cap data type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118192339) do
+ActiveRecord::Schema.define(version: 20140125191757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20140118192339) do
     t.string   "symbol"
     t.string   "sector"
     t.decimal  "price"
-    t.string   "market_cap"
+    t.decimal  "market_cap",                                         precision: 9, scale: 0
     t.string   "stock_exchange"
     t.datetime "created_at"
     t.datetime "updated_at"
